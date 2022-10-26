3c6c05f9e412e3d3583432ef32ae18b6490968d5
Adriano Anadinho <adrianoanadinho@gmail.com>
2022-10-26 15:10:01 -0300

    add button now creates items, which can be checked via click

--- lib/main.dart
+++ lib/main.dart
@@ -39,14 +39,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  TextEditingController nomeItemCont = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context1) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
@@ -57,18 +53,54 @@ class _MyHomePageState extends State<MyHomePage> {
           final item = widget.items[index];
 
           return SizedBox(
-            height: 50,
+            // height: 50,
             child: CheckboxListTile(
               title: Text(item.title.toString()),
               value: item.done,
-              onChanged: (value) {},
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+              },
               key: Key(item.title.toString()),
             ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        widget.items
+                            .add(Item(done: false, title: nomeItemCont.text));
+                        nomeItemCont.clear();
+                        Navigator.of(context).pop();
+                        ScaffoldMessenger.of(context1).showSnackBar(
+                          SnackBar(
+                            backgroundColor: Colors.green,
+                            content: Text('Item inserido com sucesso'),
+                          ),
+                        );
+                      });
+                    },
+                    child: Text('Add'),
+                  )
+                ],
+                title: Text('Adicione um item'),
+                content: TextFormField(
+                  controller: nomeItemCont,
+                  decoration: InputDecoration(labelText: 'Nome do item'),
+                ),
+              );
+            },
+          );
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
